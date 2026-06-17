ae9b069a21e173a492c30c75ca8050050ea98d38
SOUGOU Elhadji <sougoumalick@gmail.com>
2026-06-17 19:10:13 +0000

    fix: some issue

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,12 @@ import app from "./app";
 
 const PORT = 3000;
 
-
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`===================================================`);
-    console.log(`🚀 HOME NET Transport Server Running on port: ${PORT}`);
-    console.log(`👉 http://localhost:${PORT}`);
-    console.log(`===================================================`);
-  });
-
-
-
+app.get("/", (req, res) => {
+  res.send("Le backend fonctionne parfaitement sur Railway !");
+});
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`===================================================`);
+  console.log(`🚀 HOME NET Transport Server Running on port: ${PORT}`);
+  console.log(`👉 http://localhost:${PORT}`);
+  console.log(`===================================================`);
+});
